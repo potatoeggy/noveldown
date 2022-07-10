9d1812c529a348c51cd110b0333fb9e4b46f3f40
eggy <eggyrules@gmail.com>
2022-07-10 13:18:33 -0400

    feat: add metadata

--- noveldown/utils.py
+++ noveldown/utils.py
@@ -7,6 +7,35 @@ from ebooklib import epub
 
 from .sources.base_source import BaseSource, Chapter
 
+STYLE_CSS = """
+body { background-color: #ffffff;
+text-align: justify;
+margin: 2%;
+adobe-hyphenate: none; }
+pre { font-size: x-small; }
+h1 { text-align: center; }
+h2 { text-align: center; }
+h3 { text-align: center; }
+h4 { text-align: center; }
+h5 { text-align: center; }
+h6 { text-align: center; }
+.CI {
+text-align:center;
+margin-top:0px;
+margin-bottom:0px;
+padding:0px;
+}
+.center   {text-align: center;}
+.cover    {text-align: center;}
+.full     {width: 100%; }
+.quarter  {width: 25%; }
+.smcap    {font-variant: small-caps;}
+.u        {text-decoration: underline;}
+.bold     {font-weight: bold;}
+.big { font-size: larger; }
+.small { font-size: smaller; }
+"""
+
 
 def create_epub(source: BaseSource, path: Path | str) -> None:
     path = Path(path)
@@ -15,6 +44,12 @@ def create_epub(source: BaseSource, path: Path | str) -> None:
     book.set_identifier(source.id)
     book.set_title(source.title)
     book.set_language("en")
+
+    book.add_metadata("dc", "description", source.description)
+    book.add_metadata("dc", "contributor", "noveldown")
+    book.add_metadata("dc", "source", source.url)
+    for genre in source.genres:
+        book.add_metadata("dc", "subject", genre)
 
     for author in source.authors:
         book.add_author(author)
@@ -47,6 +82,11 @@ def create_epub(source: BaseSource, path: Path | str) -> None:
                         content=chap.content,
                     )
                 )
+
+    style = epub.EpubItem(
+        uid="style", file_name="style.css", media_type="text/css", content=STYLE_CSS
+    )
+    book.add_item(style)
 
     book.spine = ["nav", *chapter_htmls]
     if source.cover_url:
